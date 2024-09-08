67530bd93518c64b2576ddfe8a15ffd4fcacdacc
yudhaph0300 <yudhapamungkas0300@gmail.com>
2024-09-09 00:49:29 +0700

    add admin view

---
+++ resources/views/pages/admin/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="main-content">
+        <div class="app-content">
+            <div class="app-content-header shadow-sm">
+                <h1 class="app-content-headerText raleway fw-bold">Dashboard Admin</h1>
+            </div>
+
+            <div class="content-body">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nemo, iusto ab magnam quod rem inventore,
+                    tempora numquam beatae deserunt adipisci veritatis! Eum, voluptatum laboriosam iste similique culpa
+                    mollitia numquam sunt nostrum, recusandae ullam harum voluptatibus? Minima, illum tempora? Beatae
+                    blanditiis dolorum sunt ipsum nostrum doloremque consectetur harum, error, laboriosam reiciendis nobis
+                    itaque eos, quis maiores nulla placeat nemo repellat modi quidem perferendis culpa sapiente doloribus?
+                    Dicta placeat officiis veritatis quam enim doloribus sed magni, maiores quisquam, voluptatum ipsum
+                    eligendi suscipit quaerat voluptatem necessitatibus dignissimos repudiandae atque amet. Soluta cumque
+                    veniam libero aliquam quaerat saepe ipsa aperiam qui id optio.
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,30 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-header">
+        <div class="app-icon">
+            <h5>SMP LUBABUL FATTAH</h5>
+        </div>
+    </div>
+    <ul class="sidebar-list">
+        <li class="sidebar-list-item active">
+            <a href="/admin">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="sidebar-list-item ">
+            <a href="/admin">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="sidebar-list-item ">
+            <a href="/admin">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+
+    </ul>
+
+</div>
