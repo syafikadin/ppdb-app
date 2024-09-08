f147fc1e23d92e5ecd87a2070e40b86eafb25cdc
yudhaph0300 <yudhapamungkas0300@gmail.com>
2024-09-09 00:49:40 +0700

    update web route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardAdminController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::resource('/login', AuthController::class);
+
+Route::resource('/admin', DashboardAdminController::class);
