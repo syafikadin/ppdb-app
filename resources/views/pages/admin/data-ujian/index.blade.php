749220d29dc6956aa07b697652f2a1e3f1368e1f
yudhaph0300 <yudhapamungkas0300@gmail.com>
2024-09-13 16:42:48 +0700

    Add/Fix view

--- resources/views/pages/admin/data-ujian/index.blade.php
+++ resources/views/pages/admin/data-ujian/index.blade.php
@@ -64,9 +64,43 @@
 
                         <hr>
                         <div class="mt-3">
-                            <button class="btn btn-outline-primary">
+                            <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                 Tambah Gelombang
                             </button>
+                        </div>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                                <!-- Center the modal vertically -->
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal Tambah Gelombang</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+
+                                        <div class="mb-3">
+                                            <label for="exampleFormControlTextarea1" class="form-label small">Nama
+                                                Gelombang</label>
+                                            <input type="text" class="form-control" placeholder="Masukan nama gelombang"
+                                                required value="">
+                                        </div>
+
+                                    </div>
+                                    <div class="modal-footer ">
+
+                                        <div>
+                                            <button type="button" class="btn btn-secondary"
+                                                data-bs-dismiss="modal">Batal</button>
+                                            <button type="button" class="btn btn-primary">Simpan</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
